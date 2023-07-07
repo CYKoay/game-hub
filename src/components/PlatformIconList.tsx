92be21a0558222aaadbad20e8221ee9f766a315f
Koay Chun Yong <koaychunyong@gmail.com>
2023-07-07 09:05:48 +0800

    Correct import for react icons bs

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { Platform } from "../hooks/useGames";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import {
   FaWindows,
   FaPlaystation,
@@ -10,7 +10,7 @@ import {
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/Bs";
+import { BsGlobe } from "react-icons/bs";
 import { IconType } from "react-icons";
 
 interface Props {
